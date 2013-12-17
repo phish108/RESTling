3a455a8f700d92f86a2da3342dabf06c7e6d4377
phish108 <cpglahn@gmail.com>
2013-12-17 10:59:09 +0100

    Documentation fixes and consistency
    + corrected the class name in the documentation
    + more actual documentation
    + made Fatal Errors more visible in the logs.
    + early debug mode checking in standard logging.

--- classes/Logger.class.php
+++ classes/Logger.class.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * CoreService
+ * @class Logger
  *
- * Basic functions for logging
+ * The Logger class provides some logging functions that should simplify the typical debugging tasks. 
  */
 class Logger
 {
-    protected $debugMode = true;
+    private $debugMode = true; ///< internal variable for activating or deactivating log messages. 
 
     /**
      * setDebugMode($mode)
      *
-     * Sets the internal debug mode gor logging messages and stuff.
+     * Sets the internal debug mode for logging messages and stuff.
      *
-     * @param $mode boolean value to switch logging on or off.
+     * @param bool $mode boolean value to switch logging on or off.
      */
     public function setDebugMode($mode)
     {
@@ -26,12 +26,14 @@ class Logger
      *
      * @param string $message the error message to send
      *
-     * This helper function works pretty much like log() but it ignores the debug flag.
+     * This helper method works pretty much like log() but it ignores the debug flag.
      * This means that any fatal message will appear in the server's error log regardless
      * of the debug flag.
      *
-     * This is useful to send important error messages if we run into a non-recoverable
-     * error.
+     * This method is useful to send important error messages if some code runs into a non-recoverable
+     * error that is mission critical and points towards a critical system error.
+     *
+     * This function should be used with great care!
      */
     public function fatal($message)
     {
@@ -48,11 +50,11 @@ class Logger
 
         if (!empty($c['class']))
         {
-            error_log($c['class'] . "::".  $c['function'] . " " . $message);
+            error_log($c['class'] . "::".  $c['function'] . " FATAL ERROR: " . $message);
         }
         else
         {
-            error_log($c['function']. " " . $message);
+            error_log($c['function']. " FATAL ERROR: " . $message);
         }
     }
 
@@ -61,24 +63,26 @@ class Logger
      *
      * @param string $message takes the logging message.
      *
-     * This helper method eases the debugging of the service. It expands the message with the
-     * current class name, so it can be easily identified.
+     * This helper method eases the debugging of a class by generating log entries in the server's error logs. 
+     * It expands the message with the current class name and method name, so it can be easily identified.
+     * 
+     * This method only generates log entries if debugging is activated. 
      */
     public function log($message)
     {
-        $t = debug_backtrace();
-        // need to shift twice
-        array_shift($t); // ignore self
-        $c = array_shift($t);
-
-        // because mark() uses this function
-        if (__CLASS__ === $c['class'])
-        {
-            $c = array_shift($t);
-        }
-
         if ( $this->debugMode )
         {
+            $t = debug_backtrace();
+            // need to shift twice
+            array_shift($t); // ignore self
+            $c = array_shift($t);
+    
+            // because mark() uses this function
+            if (__CLASS__ === $c['class'])
+            {
+                $c = array_shift($t);
+            }
+       
             if (!empty($c['class']))
             {
                 error_log($c['class'] . "::".  $c['function'] . " " . $message);
@@ -97,6 +101,8 @@ class Logger
      *
      * This method sets a marker to the caller function so one can easily track down important parts in the code.
      * In the error_log file the classname and the function name will appear next with the lable 'MARK'.
+     *
+     * The typical use of this method is to quickly generate debugging markers in the code.
      */
     public function mark($extra = "")
     {
@@ -125,7 +131,8 @@ class Logger
      * @param bool $bTest test this boolean
      * @param string $message the log message
      *
-     * This helper function logs only of the $bTest parameter is TRUE.
+     * This helper function logs only of the $bTest parameter is TRUE. This function is very useful if 
+     * the log messages should appear only under certain conditions (e.g., if a variable is out of range).
      */
     public function logtest($bTest, $message)
     {
