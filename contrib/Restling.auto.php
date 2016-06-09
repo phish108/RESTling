382be5bfed2f83344956c0eebacf82475b50870f
Christian Glahn <cpglahn@gmail.com>
2016-06-09 07:56:28 +0200

    better autoloader

--- contrib/Restling.auto.php
+++ contrib/Restling.auto.php
@@ -5,32 +5,39 @@
      *
      * In order to use autoloading this file needs to be loaded during the
      * initialization of your root script.
+     *
+     * This autoloader uses namespaces and will find also classes within other
+     * namespace schemes.
      */
     spl_autoload_register(function ($class) {
+        $class = ltrim($class, '\\');
         $parts = explode('\\', $class);
-        $NSRoot = array_shift($parts);
 
-        if (isset($NSRoot) &&
-            !empty($NSRoot) &&
-            $NSRoot == "RESTling") {
+        $root = array_shift($parts);
 
-            $path = 'classes/' . end($parts) . '.class.php';
+        if (isset($root) && !empty($root)) {
+            $cpath = array();
+            // direct namespace
+            $cpath[] = $root . "/" . implode("/", $parts) . ".class.php";
+
+            // sub-directory namespaces
+            $cpath[] = $root . "/classes/" . implode("/", $parts) . ".class.php";
+            $cpath[] = $root . "/src/" . implode("/", $parts) . ".class.php";
+            $cpath[] = $root . "/lib/" . implode("/", $parts) . ".class.php";
+
+            // for developer prefixed namespaces
+            $root = array_shift($parts);
+            $cpath[] = strtolower($root) . "/src/" . implode("/", $parts) . ".class.php";
+            $cpath[] = strtolower($root) . "/lib/" . implode("/", $parts) . ".class.php";
 
             $prefixes = explode(PATH_SEPARATOR, get_include_path());
 
-            array_push($prefixes, "..");
             foreach ( $prefixes as $p ) {
-                if (file_exists($p . "/RESTling/" . $path)) {
-                    include_once $p . "/RESTling/" . $path;
-                    break;
-                }
-                else if (file_exists($p . "/include/RESTling/" . $path)) {
-                   include_once $p . "/include/RESTling/" . $path;
-                   break;
-                }
-                else if (file_exists($p . "/" . $path)) {
-                   include_once $p . "/" . $path;
-                   break;
+                foreach ($cpath as $path) {
+                    if (file_exists($p . "/" . $path)) {
+                        include_once $p . "/" . $path;
+                        break 2;
+                    }
                 }
             }
         }
