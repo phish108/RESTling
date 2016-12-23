cef39182ea5f63d9fcfccde2da79b1ba73aa8b2d
Christian Glahn <cpglahn@gmail.com>
2016-12-23 10:28:19 +0100

    changed Model interface into abstract class
    makes simple Models easier to implement

--- src/Model.php
+++ src/Model.php
@@ -1,11 +1,30 @@
 <?php
 namespace RESTling;
 
-interface Model {
-    public function getHeaders();
-    public function hasData();
-    public function getData();
-    public function getAllErrors();
+abstract class Model {
+    protected $data;
+
+    public function __construct(){}
+
+    public function getHeaders() {
+        return [];
+    }
+    public function getAllErrors() {
+        return [];
+    }
+
+    public function hasData()
+    {
+        return !empty($this->data);
+    }
+
+    public function getData()
+    {
+        $d = $this->data;
+        $this->data = null;
+
+        return $d;
+    }
 }
 
 ?>
