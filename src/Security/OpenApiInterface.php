7dc55b1166de103250839dc5a35c183886d1b637
Christian Glahn <cpglahn@gmail.com>
2016-12-27 01:16:37 +0100

    link the OAI security interface back to the core security interface

--- src/Security/OpenApiInterface.php
+++ src/Security/OpenApiInterface.php
@@ -1,7 +1,7 @@
 <?php
 namespace RESTling\Security;
 
-interface OpenApiInterface {
+interface OpenApiInterface extends \RESTling\SecurityInterface {
     public function setScheme($scheme);
     public function has($property, $valueSet = null);
     public function get($property);
