ce48424f9ec8f92b1ac14d96e99e8ab2246e5b65
Christian Glahn <cpglahn@gmail.com>
2016-12-27 15:05:27 +0100

    Interface documentation

--- src/Interfaces/Validator.php
+++ src/Interfaces/Validator.php
@@ -2,8 +2,30 @@
 namespace RESTling\Interfaces;
 
 interface Validator {
-    public function validate($model, $input);
-    public function passes();
+    /**
+ 	 * Executes the validation routine.
+     *
+     * If the validation failes, an implementation is expected to throw an
+     * Exception.
+     *
+     * A validator may decide not to validate a request.
+ 	 *
+ 	 * @param mixed $model
+     * @param \RESTling\Interface\Input
+ 	 * @return void
+	 */
+	public function validate($model, $input);
+
+    /**
+ 	 * Returns true if the validation has succeeded.
+     *
+     * If no exception was raised and this function returns false, then
+     * no validation took place. This allows to have multiple cascading
+     * validators. 
+ 	 *
+ 	 * @return bool
+	 */
+	public function passes();
 }
 
 ?>
