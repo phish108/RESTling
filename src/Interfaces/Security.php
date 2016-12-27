14b6a7834e152b42befa1d448e70155449857bef
Christian Glahn <cpglahn@gmail.com>
2016-12-27 18:42:03 +0100

    Interface documentation

--- src/Interfaces/Security.php
+++ src/Interfaces/Security.php
@@ -2,7 +2,26 @@
 namespace RESTling\Interfaces;
 
 interface Security extends Validator {
-    public function verify($model, $input);
-    public function setScopes($scopeSet);
+    /**
+ 	 * Verifies the functional authororization within a scope.
+     *
+     * A verification must raise an exception if the scope is violated.
+     *
+     * A verification may ignore the verification request in a given
+     * $model/$input setting.
+ 	 *
+ 	 * @param mixed $model
+     * @param \RESTling\Interfaces\Input
+ 	 * @return void
+	 */
+	public function verify($model, $input);
+
+    /**
+ 	 * Sets the authorization scope for the current request.
+ 	 *
+ 	 * @param array $scopeSet
+ 	 * @return void
+	 */
+	public function setScopes($scopeSet);
 }
 ?>
