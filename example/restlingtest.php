60f7ecac3fd8ea802734843fd407f670adbe094f
Christian Glahn <cpglahn@gmail.com>
2017-01-13 10:28:09 +0100

    security examples

---
+++ example/restlingtest.php
@@ -0,0 +1,43 @@
+<?php
+
+// inherit data handling functions from RESTling\Model
+// for complex data processing you may want to override the
+// hasData() and getData() functions.
+// If you want to provide complex error information, you want to
+// implement
+class RestlingTest extends \RESTling\Model
+{
+    public function get($input)
+    {
+        $this->data = 'get ok ';
+
+        if ($input->hasParameter("foo", "query")) {
+            $d = $input->getParameter("foo", "query");
+            if (is_array($d)) {
+                $this->data .= "multiple " . join(", ", $d);
+            }
+            else {
+                $this->data .= "singleton $d";
+            }
+        }
+    }
+
+    public function post()
+    {
+        $this->data = 'post ok';
+    }
+
+    public function put()
+    {
+        $this->data = 'put ok';
+        throw new \RESTling\Exception\Created(); // return created error
+    }
+
+    public function delete()
+    {
+        error_log("delete");
+        throw new \RESTling\Exception\Gone(); // return Gone error
+    }
+}
+
+?>
---
+++ example/session_handling.php
@@ -0,0 +1,58 @@
+<?php
+    // this example illustrates how to work with multiple
+    // security Schemes
+require_once __DIR__."/../vendor/autoload.php";
+
+require_once __DIR__ ."/restlingtest.php";
+
+// The preferred method is to implement a separate security model.
+
+// For very simple services and security requirements  the security accessors
+// could be directly implemented into the service class. This makes sense, for
+// example, if only API keys in Bearer Tokens are validated.
+class RestlingTestSecurity implements \RESTling\Interfaces\Security\Model
+{
+    // ***********************************************
+    // Security accessors
+    // ***********************************************
+
+    public function getCookieName() {
+        return "mySession"; // optional, default == "session"
+    }
+
+    // for Cookie authorization
+    public function validateCookie($sessionid) {
+        // we need to check if we find the cookie
+        if ($sessionid != "helloCookie") {
+            throw new \RESTling\Exception\Forbidden();
+        }
+    }
+
+    // for Bearer Authorization
+    public function validateToken($sessiontoken) {
+        if ($sessionid != "helloToken") {
+            throw new \RESTling\Exception\Forbidden();
+        }
+    }
+    
+    // for basic authorization
+    public function validateUserPassword($user, $passwd) {
+        if ($user != "foo") {
+            throw new \RESTling\Exception\Forbidden();
+        }
+        if ($passw != "bar") {
+            throw new \RESTling\Exception\Forbidden();
+        }
+    }
+}
+
+$service = new RESTling\Service();
+
+$service->setSecurityModel(new RestlingTestSecurity());
+
+$service->addSecurityHandler(new \RESTling\Security\Cookie());
+$service->addSecurityHandler(new \RESTling\Security\Bearer());
+$service->addSecurityHandler(new \RESTling\Security\Basic());
+
+$service->run(new RestlingTest());
+?>
